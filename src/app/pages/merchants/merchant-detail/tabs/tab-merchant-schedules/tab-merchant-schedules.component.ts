d5a697e40685063e39d533d4839eaa155bbf58b5
Armando <armando14694@hotmail.com>
2021-01-25 08:05:30 -0600

    Add dashboard and changes in schedules

--- src/app/pages/merchants/merchant-detail/tabs/tab-merchant-schedules/tab-merchant-schedules.component.ts
+++ src/app/pages/merchants/merchant-detail/tabs/tab-merchant-schedules/tab-merchant-schedules.component.ts
@@ -7,12 +7,11 @@ import { MerchantsService } from '../../../../../services/merchants.service';
   styleUrls: ['./tab-merchant-schedules.component.scss']
 })
 export class TabMerchantSchedulesComponent implements OnInit {
-
   schedules: Array<any> = [];
 
   constructor(private merchantsService: MerchantsService) {
     this.schedules = this.merchantsService.merchantId.schedules;
-    console.log('Schedules by merchant', this.schedules);
+    // console.log('Schedules by merchant', this.schedules);
   }
 
   ngOnInit(): void {
